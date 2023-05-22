418712ce4736c01b68c77df39d83c4e1e3dfeb46
Leonardo Piovesan <leopvsn@gmail.com>
2023-05-21 22:06:01 -0300

    feat: add dependencies, model prisma & config eslint

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,7 +1,9 @@
-import fastify from "fastify";
+import fastify from 'fastify'
 
 const app = fastify()
 
-app.listen({
-    port: 3333
-}).then(() => console.log('http server running on http://localhost:3333'));
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => console.log('http server running on http://localhost:3333'))
