672397bce9d1fc7f5ac57384aa2e5f11d1ddac90
Leonardo Piovesan <leopvsn@gmail.com>
2023-11-10 22:18:09 -0300

    feat: web layout initial setup

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,7 +1,26 @@
 export default function Home() {
   return (
-    <div className="h-screen bg-zinc-950 text-zinc-50 p-6">
-      <h1 className="text-3xl font-bold">Sua cápsula do tempo</h1>
-    </div>
-  )
+    <main className="grid min-h-screen grid-cols-2">
+      <div className="relative m-10 flex flex-col items-start justify-between overflow-hidden bg-[url(../assets/bg-stars.svg)] px-28 py-16">
+        <div className="absolute right-0 top-1/2 h-[288px] w-[526px] -translate-y-1/2 translate-x-1/2 rounded-full bg-purple-700 opacity-50 blur-full" />
+        <div className="absolute bottom-0 right-2 top-0 w-2 bg-stripes " />
+
+        <a href="" className="flex items-center gap-3 text-left">
+          <div className="flex h-10 w-10 items-center justify-center rounded-full bg-gray-400"></div>
+        </a>
+      </div>
+
+      <div className="flex flex-col bg-[url(../assets/bg-stars.svg)] bg-cover p-16">
+        <div className="flex flex-1 items-center justify-center">
+          <p className="w-[360px] text-center leading-relaxed">
+            Você ainda não registrou nenhuma lembrança, comece a{" "}
+            <a href="" className="underline hover:text-gray-50">
+              criar agora
+            </a>
+            !
+          </p>
+        </div>
+      </div>
+    </main>
+  );
 }
