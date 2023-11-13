176506fbc26f0f0f0d48946fa991d648c88976ba
Leonardo Piovesan <leopvsn@gmail.com>
2023-11-13 14:25:47 -0300

    style: add alt font and created home layout style

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -1,8 +1,16 @@
 import { ReactNode } from "react";
 import "./globals.css";
-import { Roboto_Flex as Roboto } from "next/font/google";
+import {
+  Roboto_Flex as Roboto,
+  Bai_Jamjuree as BaiJamjuree,
+} from "next/font/google";
 
 const roboto = Roboto({ subsets: ["latin"], variable: "--font-roboto" });
+const baiJamJuree = BaiJamjuree({
+  subsets: ["latin"],
+  weight: "700",
+  variable: "--font-bai-jumjuree",
+});
 
 export const metadata = {
   title: "NLW Spacetime",
@@ -14,7 +22,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <body
-        className={`${roboto.variable} bg-gray-900 font-sans text-gray-100`}
+        className={`${roboto.variable} ${baiJamJuree.variable} bg-gray-900 font-sans text-gray-100`}
       >
         {children}
       </body>
